3cd2ed50a8e2a49e88a5bbed94041fd0b6701928
Tarun Malempati <24860718+tarunmalempati@users.noreply.github.com>
2024-05-02 22:13:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,13 @@
 IMAGE_NAME=react-app
 IMAGE_VERSION=v1
 
-# Tag the image with the Docker Hub repository name and version
+#Tag the image with the Docker Hub repository name and version
 docker tag $IMAGE_NAME:$IMAGE_VERSION tarun8117/prod:$IMAGE_VERSION
 docker tag $IMAGE_NAME:$IMAGE_VERSION tarun8117/dev:$IMAGE_VERSION
 
-# Push the image to Docker Hub
+#login to docker hub  
+docker login -u tarun8117 -p    
+  
+#Push the image to Docker Hub
 docker push tarun8117/prod:$IMAGE_VERSION
 docker push tarun8117/dev:$IMAGE_VERSION
