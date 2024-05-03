bfed865dc704c6300e7bf0e8295a462a5d449b47
Tarun Malempati <24860718+tarunmalempati@users.noreply.github.com>
2024-05-03 17:36:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,4 @@
 
 #build the docker image
 docker build -t react-app .
-docker images 
-
-#building docker container
-docker-compose up -d
-docker ps
-
-echo "Container created successfully"
 echo "Build script success"
