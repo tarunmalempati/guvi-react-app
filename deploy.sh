add8ee838e74e1a4b987da5b1a4bf1c6abb686a0
Tarun Malempati <24860718+tarunmalempati@users.noreply.github.com>
2024-05-03 22:13:08 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,5 @@
 # Remove all containers
 #docker rm $(docker ps -aq)
 
-#delete previously built images
-docker rmi -f $(docker images -aq)
-
-#pull the image from docker hub
-docker pull -q tarun8117/guvi-react-app-dev:latest
-
 # Start new container with the latest image
 docker-compose up -d
