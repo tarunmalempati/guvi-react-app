c0e49139e5213e6170ed32ec4ee8b1a3c3fc3acc
Tarun Malempati <24860718+tarunmalempati@users.noreply.github.com>
2024-05-03 21:09:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+#delete previously built images
+docker rmi -f $(docker images -aq)
+
 #build the docker image
 docker build -t react-app .
 echo "Build script success"
 
 #login to docker hub
-docker login -u tarun8117 -p dckr_pat_GyI9w66zsRUPxw2DENj2REmFHEM
+#docker login -u tarun8117 -p dckr_pat_GyI9w66zsRUPxw2DENj2REmFHEM
 
 #push the image to docker hub
-docker tag react-app:latest tarun8117/guvi-react-app-dev:latest
-docker push tarun8117/guvi-react-app-dev:latest
+#docker tag react-app:latest tarun8117/guvi-react-app-dev:latest
+#docker push tarun8117/guvi-react-app-dev:latest
