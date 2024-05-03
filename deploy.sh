f27dd3752f563774d07b7748d9bc551585f106a1
Tarun Malempati <24860718+tarunmalempati@users.noreply.github.com>
2024-05-03 22:06:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@
 docker rmi -f $(docker images -aq)
 
 #pull the image from docker hub
-docker login -u tarun8117 -p dckr_pat_GyI9w66zsRUPxw2DENj2REmFHEM
-docker pull tarun8117/guvi-react-app-dev:latest
+#docker pull tarun8117/guvi-react-app-dev:latest
+yes | docker pull tarun8117/guvi-react-app-dev:latest
 
 # Start new container with the latest image
 docker-compose up -d
