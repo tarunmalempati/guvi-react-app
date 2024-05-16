4ee6009d5d8b041b2b39b177b87e02307530d14e
Tarun Malempati <24860718+tarunmalempati@users.noreply.github.com>
2024-05-16 21:18:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ docker login -u tarun8117 -p dckr_pat_GyI9w66zsRUPxw2DENj2REmFHEM
 
 #push the image to docker hub
 docker tag react-app:latest tarun8117/guvi-react-app-prod:latest
-docker push tarun8117/guvi-react-app-prod:latest  
+#docker push tarun8117/guvi-react-app-prod:latest  
